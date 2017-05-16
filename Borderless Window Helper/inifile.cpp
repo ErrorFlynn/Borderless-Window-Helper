26929095cbfb8ae455a022851bd3b35287cba135
ErrorFlynn <ErrorFlynn@users.noreply.github.com>
2017-05-16 19:19:16 -0400

    Add files via upload

---
+++ Borderless Window Helper/inifile.cpp
@@ -0,0 +1,186 @@
+#include "inifile.h"
+
+wstring IniFile::safe_fname;
+
+IniFile::IniFile(const wstring &fname) : fname(fname), sort_sections(false), sort_entries(false), nospaces(false)
+{
+	static bool first(true);
+
+	if(first)
+	{
+		first = false;
+		wstring appdata(ini_file::GetSysFolderLocation(CSIDL_APPDATA));
+		if(appdata.size()) safe_fname = appdata + fname.substr(fname.rfind('\\'));
+	}
+
+	LoadData();
+}
+
+void IniFile::LoadData()
+{
+	bool b = FileExist(safe_fname.data());
+	if(!b && !FileExist(fname.data())) return;
+
+	ifstream file(b ? safe_fname : fname, ios::ate);
+	if(!file.is_open()) return;
+	size_t size = (size_t)file.tellg();
+	if(!size) return;
+	stringstream data;
+	string data_temp(size, '\0');
+	file.seekg(0);
+	file.read(&data_temp.front(), size);
+	data << data_temp;
+	data_temp.clear();
+
+	string line, section_name, entry_name, entry_data;
+	size_t section_index;
+
+	while(!data.eof())
+	{
+		getline(data, line);
+		if(line.size() < 3 || line[0] != '[') continue;
+		section_name = line.substr(1, line.find(']', 1)-1);
+
+		bool found = false;
+		for(section_index = 0; section_index<sections.size(); section_index++)
+			if(sections[section_index].name() == section_name) { found = true; break; }
+		if(!found) 
+		{
+			sections.emplace_back(section_name);
+			section_index = sections.size() - 1;
+		}
+
+		while(!data.eof() && data.peek() != '[')
+		{
+			getline(data, line);
+			size_t pos = line.find('=');
+			if(pos == string::npos) continue;
+			entry_name = line.substr(0, pos);
+			entry_name = entry_name.substr(0, entry_name.find(' '));
+			size_t p = line.find_first_not_of(' ', pos+1);
+			entry_data = p == string::npos ? "" : line.substr(p);
+			sections[section_index].add_entry(entry_name, entry_data);
+		}
+	}
+}
+
+void IniFile::SaveData()
+{
+	if(sections.empty() || sections[0].entries.empty()) return;
+	bool b = FileExist(safe_fname.data());
+	ofstream file(b ? safe_fname : fname, ios::trunc);
+	if(!file.is_open())
+	{
+		file.open(safe_fname, ios::trunc);
+		if(!file.is_open()) return;
+	}
+	if(sort_sections) sort(sections.begin(), sections.end(), [](section a, section b) {
+		return lexicographical_compare(a.name().begin(), a.name().end(), b.name().begin(), b.name().end());
+	});
+
+	for(auto &section : sections)
+	{
+		file << '[' << section.name() << "]\n";
+		if(sort_entries) sort(section.entries.begin(), section.entries.end(), [](section::entry a, section::entry b) {
+			return lexicographical_compare(a.name().begin(), a.name().end(), b.name().begin(), b.name().end());
+		});
+
+		for(auto &entry : section.entries)
+			file << entry.name() << (nospaces ? "=" : " = ") << entry.data() << '\n';
+
+		if(section.name() != sections.back().name()) file << '\n';
+	}
+}
+
+void IniFile::section::add_entry(const string &name, const string &data)
+{
+	bool hasentry = false;
+	size_t n;
+
+	for(n=0; n<entries.size(); n++)
+		if(entries[n].name() == name) { hasentry = true; break; }
+
+	if(hasentry) entries[n].setdata(data);
+	else entries.emplace_back(name, data);
+}
+
+void IniFile::WriteInt(const string &section_name, const string& entry_name, int value)
+{
+	bool hassection = false;
+	size_t n = 0;
+
+	for(; n<sections.size(); n++)
+		if(sections[n].name() == section_name) { hassection = true; break; }
+
+	if(hassection) sections[n].add_entry(entry_name, to_string(value));
+	else 
+	{
+		sections.emplace_back(section_name);
+		sections.back().add_entry(entry_name, to_string(value));
+	}
+}
+
+void IniFile::WriteUInt(const string &section_name, const string& entry_name, unsigned value)
+{
+	bool hassection = false;
+	size_t n = 0;
+
+	for(; n<sections.size(); n++)
+		if(sections[n].name() == section_name) { hassection = true; break; }
+
+	if(hassection) sections[n].add_entry(entry_name, to_string(value));
+	else
+	{
+		sections.emplace_back(section_name);
+		sections.back().add_entry(entry_name, to_string(value));
+	}
+}
+
+void IniFile::WriteString(const string &section_name, const string& entry_name, const string &value)
+{
+	bool hassection = false;
+	size_t n = 0;
+
+	for(; n<sections.size(); n++)
+		if(sections[n].name() == section_name) { hassection = true; break; }
+
+	if(hassection) sections[n].add_entry(entry_name, value);
+	else 
+	{
+		sections.emplace_back(section_name);
+		sections.back().add_entry(entry_name, value);
+	}
+}
+
+int IniFile::ReadInt(const string &section_name, const string &entry_name, int default_value)
+{
+	for(auto &section : sections)
+		if(section.name() == section_name)
+			for(auto &entry : section.entries)
+				if(entry.name() == entry_name)
+					return stoi(entry.data());
+
+	return default_value;
+}
+
+unsigned IniFile::ReadUInt(const string &section_name, const string &entry_name, unsigned default_value)
+{
+	for(auto &section : sections)
+		if(section.name() == section_name)
+			for(auto &entry : section.entries)
+				if(entry.name() == entry_name)
+					return stoul(entry.data());
+
+	return default_value;
+}
+
+string IniFile::ReadString(const string &section_name, const string &entry_name, const string &default_value)
+{
+	for(auto &section : sections)
+		if(section.name() == section_name)
+			for(auto &entry : section.entries)
+				if(entry.name() == entry_name)
+					return entry.data();
+
+	return default_value;
+}
---
+++ Borderless Window Helper/inifile.h
@@ -0,0 +1,87 @@
+#include "shlobj.h"
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+#define FileExist(x) (GetFileAttributesW(x) != -1)
+
+using namespace std;
+
+class IniFile
+{
+	wstring fname;
+	static wstring safe_fname;
+	bool sort_sections, sort_entries, nospaces;
+
+	void LoadData();
+	void SaveData();
+
+	class section
+	{
+		string _name;
+
+		class entry
+		{
+			string _name;
+			string _data;
+
+		public:
+			entry(const string &name, const string &data) : _name(name), _data(data) {};
+			const string& name() { return _name; }
+			const string& data() { return _data; }
+			void setdata(const string& d) { _data = d; }
+		};
+		
+		vector<entry> entries;
+
+	public:
+		friend class IniFile;
+		section(const string &name) : _name(name) {};
+		const string& name() { return _name; }
+		void add_entry(const string&, const string&);
+	};
+	
+	vector<section> sections;
+
+public:
+
+	IniFile(const wstring &fname);
+	~IniFile() { SaveData(); }
+
+	void Clear() { sections.clear(); }
+	void Refresh() { Clear(); LoadData(); }
+	void SortSections() { sort_sections = true; }
+	void SortEntries() { sort_entries = true; }
+	void Sort() { SortSections(); SortEntries(); }
+	void NoSpaces() { nospaces = true; }
+	
+	int ReadInt(const string &section_name, const string &entry_name, int default_value);
+	unsigned ReadUInt(const string &section_name, const string &entry_name, unsigned default_value);
+	string ReadString(const string &section_name, const string &entry_name, const string &default_value);
+	void WriteInt(const string &section_name, const string &entry_name, int value);
+	void WriteUInt(const string &section_name, const string &entry_name, unsigned value);
+	void WriteString(const string &section_name, const string &entry_name, const string &value);
+};
+
+namespace ini_file
+{
+	static wstring GetSysFolderLocation(int csidl)
+	{
+		LPITEMIDLIST pidl;
+		if(SHGetFolderLocation(NULL, csidl, NULL, NULL, &pidl) == S_OK)
+		{
+			WCHAR path[_MAX_PATH];
+			BOOL ret = SHGetPathFromIDListW(pidl, path);
+			LPMALLOC pMalloc;
+			if(SHGetMalloc(&pMalloc) == S_OK)
+			{
+				pMalloc->Free(pidl);
+				pMalloc->Release();
+			}
+			if(ret) return path;
+		}
+		return L"";
+	}
+}
