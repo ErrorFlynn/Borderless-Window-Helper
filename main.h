2f36b0e45ab453915a601e6393b733a7b8b07155
ErrorFlynn <ErrorFlynn@users.noreply.github.com>
2017-06-09 01:25:28 -0400

    Add files via upload

---
+++ main.h
@@ -0,0 +1,57 @@
+#include <nana/gui/widgets/listbox.hpp>
+#include <nana/gui.hpp>
+#include <nana/gui/timer.hpp>
+#include <nana/gui/widgets/label.hpp>
+
+#include "util.h"
+#include <string>
+#include <map>
+
+#pragma warning( disable : 4800 4267 4996)
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+	processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#define TITLE "Borderless Window Helper 1.2"
+#define TITLEW L"Borderless Window Helper 1.2"
+
+using namespace std;
+using namespace nana;
+
+string last;
+wstring inifile;
+filepath self_path;
+HWND hwnd;
+bool mintray(true);
+
+struct monwin
+{
+	int style = 0;
+	bool active = false;
+	string pname; // process name as displayed in the list (not lowercased)
+	monwin() {}
+	monwin(int st, bool a, string pn) { style = st, active = a; pname = pn; }
+};
+
+map<string, monwin> monwins; // key is lowercase process name
+
+struct enumwin
+{
+	DWORD procid = 0;
+	string pname; // process name as displayed in the list (not lowercased)
+	HWND hwnd = nullptr;
+	HMONITOR monitor = nullptr;
+	wstring captionw;
+	bool borderless = false;
+};
+
+map<string, enumwin> windows; // key is lowercase process name
+
+
+void LoadSettings();
+void SaveSettings();
+void RunGUI(bool show);
+void enum_windows();
+void enum_timer_fn(listbox &list1, listbox &list2, label &info);
+void mon_timer_fn();
+bool am_i_already_running();
+void sort_list(listbox&);
---
+++ util.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include <chrono>
+
+#pragma warning( disable : 4800 4267 4996)
+
+#ifndef FileExist
+#define FileExist(x) (GetFileAttributesA(x) != -1)
+#endif
+#define FileExistW(x) (GetFileAttributesW(x) != -1)
+
+using namespace std;
+
+
+class filepath
+{
+private:
+	wstring pathw_, dirw_, namew_, extw_, fullnamew_;
+	string path_, dir_, name_, ext_, fullname_;
+
+public:
+	filepath() {}
+	filepath(string);
+	filepath(wstring);
+	string path() { return path_; }
+	wstring pathw() { return pathw_; }
+	string dir() { return dir_; }
+	wstring dirw() { return dirw_; }
+	string name() { return name_; }
+	wstring namew() { return namew_; }
+	string ext() { return ext_; }
+	wstring extw() { return extw_; }
+	string fullname() { return fullname_; }
+	wstring fullnamew() { return fullnamew_; }
+};
+
+
+// UTF8 conversion of wide character string (std::wstring) to multibyte string (std::string)
+static void wctomb(const wstring &wcstr, string &mbstr, unsigned cp = CP_UTF8)
+{
+	if(wcstr.empty()) return;
+	int len = WideCharToMultiByte(cp, NULL, wcstr.data(), -1, nullptr, 0, NULL, NULL);
+	mbstr.assign(len-1, '\0');
+	WideCharToMultiByte(cp, NULL, wcstr.data(), -1, &mbstr.front(), len-1, NULL, NULL);
+}
+
+// UTF8 conversion of multibyte string (std::string) to wide character string (std::wstring)
+static void mbtowc(const string &mbstr, wstring &wcstr, unsigned cp = CP_UTF8)
+{
+	if(mbstr.empty()) return;
+	int len = MultiByteToWideChar(cp, NULL, mbstr.data(), -1, nullptr, 0);
+	wcstr.assign(len-1, '\0');
+	MultiByteToWideChar(cp, NULL, mbstr.data(), -1, &wcstr.front(), len-1);
+}
+
+static string strlower(string s) { for(auto &c : s) c = tolower(c); return move(s); }
+static wstring strlower(wstring s) { for(auto &c : s) c = tolower(c); return move(s); }
+
+LONGLONG GetFileSize(LPCWSTR);
+string GetLastErrorStr();
+wstring GetLastErrorStrW();
+string to_hex_string(unsigned);
+// NO trailing backslash
+wstring GetAppFolder();
+filepath AppPath();
+wstring MakeTempFolder(wstring);
+
+
+class chronometer
+{
+	long long start, elapsed_;
+	bool stopped;
+	struct time { long long h, m, s; };
+
+public:
+	chronometer() { reset(); }
+	void stop() { stopped = true; elapsed_ = (chrono::system_clock::now().time_since_epoch().count()-start)/10000; }
+	void reset() { stopped = false; elapsed_ = 0; start = chrono::system_clock::now().time_since_epoch().count(); }
+
+	long long elapsed_ms()
+	{ 
+		if(stopped) return elapsed_;
+		else return (chrono::system_clock::now().time_since_epoch().count()-start)/10000;
+	}
+
+	long long elapsed_s() { return elapsed_ms()/1000; }
+
+	time elapsed()
+	{
+		auto es = elapsed_s();
+		return { es/3600, (es%3600)/60, (es%3600)%60 };
+	}
+};
