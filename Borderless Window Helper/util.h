71cc0f755e7c776b1854c8b0b820e1a9de0715b3
oltolm <oleg.tolmatcev@gmail.com>
2021-01-16 13:31:47 +0100

    remove wctomb and mbtowc

--- Borderless Window Helper/util.h
+++ Borderless Window Helper/util.h
@@ -9,24 +9,6 @@
 using namespace std;
 
 
-// UTF8 conversion of wide character string (std::wstring) to multibyte string (std::string)
-static void wctomb(const wstring &wcstr, string &mbstr, unsigned cp = CP_UTF8)
-{
-	if(wcstr.empty()) return;
-	int len = WideCharToMultiByte(cp, 0, wcstr.data(), -1, nullptr, 0, NULL, NULL);
-	mbstr.assign(len-1, '\0');
-	WideCharToMultiByte(cp, 0, wcstr.data(), -1, &mbstr.front(), len-1, NULL, NULL);
-}
-
-// UTF8 conversion of multibyte string (std::string) to wide character string (std::wstring)
-static void mbtowc(const string &mbstr, wstring &wcstr, unsigned cp = CP_UTF8)
-{
-	if(mbstr.empty()) return;
-	int len = MultiByteToWideChar(cp, 0, mbstr.data(), -1, nullptr, 0);
-	wcstr.assign(len-1, '\0');
-	MultiByteToWideChar(cp, 0, mbstr.data(), -1, &wcstr.front(), len-1);
-}
-
 static string strlower(string s) { for(auto &c : s) c = tolower(c); return s; }
 static wstring strlower(wstring s) { for(auto &c : s) c = tolower(c); return s; }
 
