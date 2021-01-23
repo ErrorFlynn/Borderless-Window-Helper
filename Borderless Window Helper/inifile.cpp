bfacdc0d69e8666c0b1bc17208999a86f49e8502
oltolm <oleg.tolmatcev@gmail.com>
2021-01-23 17:46:18 +0100

    improve ini file handling

--- Borderless Window Helper/inifile.cpp
+++ Borderless Window Helper/inifile.cpp
@@ -33,28 +33,35 @@ void IniFile::LoadData()
 	while(!data.eof())
 	{
 		getline(data, line);
-		if(line.size() < 3 || line[0] != '[') continue;
+		if(line.size() < 3 || line[0] != '[')
+			continue;
 		section_name = line.substr(1, line.find(']', 1)-1);
 
 		bool found = false;
-		for(section_index = 0; section_index<sections.size(); section_index++)
-			if(sections[section_index].name() == section_name) { found = true; break; }
+		for(const auto& section : sections)
+		{
+			if(section.name() == section_name)
+			{
+				found = true;
+				break;
+			}
+		}
 		if(!found) 
 		{
 			sections.emplace_back(section_name);
-			section_index = sections.size() - 1;
 		}
 
 		while(!data.eof() && data.peek() != '[')
 		{
 			getline(data, line);
 			size_t pos = line.find('=');
-			if(pos == string::npos) continue;
+			if(pos == string::npos)
+				continue;
 			entry_name = line.substr(0, pos);
 			entry_name = entry_name.substr(0, entry_name.find(' '));
 			size_t p = line.find_first_not_of(' ', pos+1);
 			entry_data = p == string::npos ? "" : line.substr(p);
-			sections[section_index].add_entry(entry_name, entry_data);
+			sections.back().add_entry(entry_name, entry_data);
 		}
 	}
 }
@@ -71,41 +78,43 @@ void IniFile::SaveData()
 	ofstream file(fname, ios::trunc);
 	if(!file.is_open())
 		return;
-	if(sort_sections) sort(sections.begin(), sections.end(), sectionComparator);
+	if(sort_sections)
+		sort(sections.begin(), sections.end(), sectionComparator);
 
 	for(auto &section : sections)
 	{
 		file << '[' << section.name() << "]\n";
-		if(sort_entries) sort(sections.begin(), sections.end(), sectionComparator);
+		if(sort_entries)
+			sort(sections.begin(), sections.end(), sectionComparator);
 
 		for(auto &entry : section.entries)
 			file << entry.name() << (nospaces ? "=" : " = ") << entry.data() << '\n';
 
-		if(section.name() != sections.back().name()) file << '\n';
+		if(section.name() != sections.back().name())
+			file << '\n';
 	}
 }
 
 void IniFile::section::add_entry(const string &name, const string &data)
 {
-	bool hasentry = false;
-	size_t n;
+	auto it = std::find_if(entries.begin(), entries.end(), [&name](auto& entry) {
+		return entry.name() == name;
+	});
 
-	for(n=0; n<entries.size(); n++)
-		if(entries[n].name() == name) { hasentry = true; break; }
-
-	if(hasentry) entries[n].setdata(data);
-	else entries.emplace_back(name, data);
+	if(it != entries.end())
+		it->setdata(data);
+	else
+		entries.emplace_back(name, data);
 }
 
 void IniFile::WriteInt(const string &section_name, const string& entry_name, int value)
 {
-	bool hassection = false;
-	size_t n = 0;
+	auto it = std::find_if(sections.begin(), sections.end(), [&section_name](auto& section) {
+		return section.name() == section_name;
+	});
 
-	for(; n<sections.size(); n++)
-		if(sections[n].name() == section_name) { hassection = true; break; }
-
-	if(hassection) sections[n].add_entry(entry_name, to_string(value));
+	if(it != sections.end())
+		it->add_entry(entry_name, to_string(value));
 	else 
 	{
 		sections.emplace_back(section_name);
@@ -115,13 +124,12 @@ void IniFile::WriteInt(const string &section_name, const string& entry_name, int
 
 void IniFile::WriteUInt(const string &section_name, const string& entry_name, unsigned value)
 {
-	bool hassection = false;
-	size_t n = 0;
+	auto it = std::find_if(sections.begin(), sections.end(), [&section_name](auto& section) {
+		return section.name() == section_name;
+	});
 
-	for(; n<sections.size(); n++)
-		if(sections[n].name() == section_name) { hassection = true; break; }
-
-	if(hassection) sections[n].add_entry(entry_name, to_string(value));
+	if(it != sections.end())
+		it->add_entry(entry_name, to_string(value));
 	else
 	{
 		sections.emplace_back(section_name);
@@ -131,13 +139,12 @@ void IniFile::WriteUInt(const string &section_name, const string& entry_name, un
 
 void IniFile::WriteString(const string &section_name, const string& entry_name, const string &value)
 {
-	bool hassection = false;
-	size_t n = 0;
+	auto it = std::find_if(sections.begin(), sections.end(), [&section_name](auto& section) {
+		return section.name() == section_name;
+	});
 
-	for(; n<sections.size(); n++)
-		if(sections[n].name() == section_name) { hassection = true; break; }
-
-	if(hassection) sections[n].add_entry(entry_name, value);
+	if(it != sections.end())
+		it->add_entry(entry_name, value);
 	else 
 	{
 		sections.emplace_back(section_name);
