8ca361ae930f8ae263f3bd58ef6e78063b2aac0f
oltolm <oleg.tolmatcev@gmail.com>
2021-03-10 20:15:05 +0100

    update CMakeLists.txt

--- Borderless Window Helper/cmake/cv2pdb.cmake
+++ Borderless Window Helper/cmake/cv2pdb.cmake
@@ -6,13 +6,13 @@ else()
   return()
 endif()
 
-macro(add_cv2pdb_target pdb_target)
+macro(add_cv2pdb_target pdb_target ext)
   if(CV2PDB)
     file(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/${pdb_target}/")
     foreach(t IN ITEMS ${ARGN})
       add_custom_command(
 #        OUTPUT "${pdb_target}/$<TARGET_FILE_NAME:${t}>" "${pdb_target}/$<TARGET_FILE_BASE_NAME:${t}>.pdb"
-        OUTPUT "${pdb_target}/${t}${suffix}" "${pdb_target}/${t}.pdb"
+        OUTPUT "${pdb_target}/${t}${ext}" "${pdb_target}/${t}.pdb"
         COMMAND "${CV2PDB}" "$<TARGET_FILE:${t}>" "${PROJECT_BINARY_DIR}/${pdb_target}/$<TARGET_FILE_NAME:${t}>" "${PROJECT_BINARY_DIR}/${pdb_target}/$<TARGET_FILE_BASE_NAME:${t}>.pdb"
         DEPENDS ${t}
         COMMENT "creating PDB files for target ${t}")
