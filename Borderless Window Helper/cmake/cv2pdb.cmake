94c290d0611e5c970f31d8112a797cfbe10a2c7c
oltolm <oleg.tolmatcev@gmail.com>
2021-04-08 21:21:09 +0200

    cmake: update build system

--- Borderless Window Helper/cmake/cv2pdb.cmake
+++ Borderless Window Helper/cmake/cv2pdb.cmake
@@ -1,3 +1,5 @@
+include_guard()
+
 find_program(CV2PDB cv2pdb)
 if(CV2PDB)
   message(STATUS "cv2pdb: ${CV2PDB}")
@@ -6,19 +8,14 @@ else()
   return()
 endif()
 
-macro(add_cv2pdb_target pdb_target ext)
+macro(add_cv2pdb_target ext t)
   if(CV2PDB)
-    file(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/${pdb_target}/")
-    foreach(t IN ITEMS ${ARGN})
-      add_custom_command(
-#        OUTPUT "${pdb_target}/$<TARGET_FILE_NAME:${t}>" "${pdb_target}/$<TARGET_FILE_BASE_NAME:${t}>.pdb"
-        OUTPUT "${pdb_target}/${t}${ext}" "${pdb_target}/${t}.pdb"
-        COMMAND "${CV2PDB}" "$<TARGET_FILE:${t}>" "${PROJECT_BINARY_DIR}/${pdb_target}/$<TARGET_FILE_NAME:${t}>" "${PROJECT_BINARY_DIR}/${pdb_target}/$<TARGET_FILE_BASE_NAME:${t}>.pdb"
-        DEPENDS ${t}
-        COMMENT "creating PDB files for target ${t}")
-      add_custom_target(${t}_pdb DEPENDS "${pdb_target}/$<TARGET_FILE_NAME:${t}>" "${pdb_target}/$<TARGET_FILE_BASE_NAME:${t}>.pdb")
-      list(APPEND pdb_targets ${t}_pdb)
-    endforeach()
-    add_custom_target(${pdb_target} DEPENDS ${pdb_targets})
+    add_custom_command(
+      # OUTPUT "pdb/$<TARGET_FILE_NAME:${t}>" "pdb/$<TARGET_FILE_BASE_NAME:${t}>.pdb"
+      OUTPUT "pdb/${t}${ext}" "pdb/${t}.pdb"
+      COMMAND "${CV2PDB}" "$<TARGET_FILE:${t}>" "${PROJECT_BINARY_DIR}/pdb/$<TARGET_FILE_NAME:${t}>" "${PROJECT_BINARY_DIR}/pdb/$<TARGET_FILE_BASE_NAME:${t}>.pdb"
+      DEPENDS ${t}
+      COMMENT "creating PDB files for target ${t}")
+    add_custom_target(${t}_pdb DEPENDS "pdb/$<TARGET_FILE_NAME:${t}>" "pdb/$<TARGET_FILE_BASE_NAME:${t}>.pdb")
   endif()
 endmacro()
